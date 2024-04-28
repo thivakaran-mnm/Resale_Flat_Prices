021bff0abbc7e59c7dddc32bcff0643df1d7e197
thivakaran-mnm <78253034+thivakaran-mnm@users.noreply.github.com>
2024-04-28 13:22:38 +0530

    Update Resale_Flat_Prices.py

--- Resale_Flat_Prices.py
+++ Resale_Flat_Prices.py
@@ -8,12 +8,6 @@ from streamlit_option_menu import option_menu
 import pickle
 from PIL import Image
 import os
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-gauth = GoogleAuth()
-gauth.LocalWebserverAuth()  # Creates local webserver and auto handles authentication.
-drive = GoogleDrive(gauth)
 
 def town_mapping(town_map):
     if town_map == 'ANG MO KIO':
@@ -161,12 +155,7 @@ def predict_price(year,town,flat_type,flr_area_sqm,flat_model,stry_start,stry_en
     lese_coms_dt= int(les_coms_dt)
 
     
-    file_id = '1GhrlU_sDDkSEEA3KSjJ6DG_DZ5enhG66'
-
-    file = drive.CreateFile({'id': file_id})
-    file.GetContentFile('Resale_Flat_Prices_Model_1.pkl')
-    
-    with open('Resale_Flat_Prices_Model_1.pkl',"rb") as f:
+    with open('customer_satisfaction/Resale_Flat_Prices_Model_1.pkl',"rb") as f:
         regg_model= pickle.load(f)
 
     user_data = np.array([[year_1,town_2,flt_ty_2,flr_ar_sqm_1,
